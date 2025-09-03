e3a8675d5b2ca4c94c8a76539e286e1a0e1c5c91
shanpushen <shenshanpu207@gmail.com>
2025-09-03 16:37:38 +0800

    update 16:30 03-Sept

--- START.bat
+++
@@ -1,7 +0,0 @@
-python jemdoc -c mysite.conf index.jemdoc
-python jemdoc -c mysite.conf experience.jemdoc
-python jemdoc -c mysite.conf research.jemdoc
-python jemdoc -c mysite.conf publications.jemdoc
-python jemdoc -c mysite.conf teaching.jemdoc
-python jemdoc -c mysite.conf services.jemdoc
-python jemdoc -c mysite.conf awards.jemdoc
